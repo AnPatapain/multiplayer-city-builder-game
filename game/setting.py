7cd5805f74da13f93cdaf9153913e73798ed4301
ke_An <nkalk192002@gmail.com>
2022-10-04 23:03:57 +0200

    flag in setting in an

--- game/setting.py
+++ game/setting.py
@@ -4,9 +4,6 @@ NUMS_GRID_X = 50 # number of tiles per row
 NUMS_GRID_Y = 50 # numbder of tiles per column
 TILE_SIZE = 116 # length and height of square tile
 
-DAA=1
-DAA=2
-DAA=3
-DAA=4
+flag_an_branch=1
 # TILE_SIZE = 58
 # TILE_SIZE = 29
