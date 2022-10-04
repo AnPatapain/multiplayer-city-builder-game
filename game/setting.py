8364fc58de75f376998533a58cb0ae3e4c29088c
ke_An <nkalk192002@gmail.com>
2022-10-04 22:58:34 +0200

    add 1 in main

--- game/setting.py
+++ game/setting.py
@@ -3,5 +3,7 @@ SCREEN_WIDTH = 2880
 NUMS_GRID_X = 50 # number of tiles per row 
 NUMS_GRID_Y = 50 # numbder of tiles per column
 TILE_SIZE = 116 # length and height of square tile
+
+ADD1=1
 # TILE_SIZE = 58
 # TILE_SIZE = 29
