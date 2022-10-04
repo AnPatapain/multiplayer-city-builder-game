159d0e6273ecd6e73456bf4f4bc551a070284a25
ke_An <nkalk192002@gmail.com>
2022-10-04 22:39:25 +0200

    change in an-branch

--- game/setting.py
+++ game/setting.py
@@ -1,7 +1,7 @@
 SCREEN_HEIGHT = 1800
 SCREEN_WIDTH = 2880
-NUMS_GRID_X = 30 # number of tiles per row 
-NUMS_GRID_Y = 30 # numbder of tiles per column
+NUMS_GRID_X = 20 # number of tiles per row 
+NUMS_GRID_Y = 20 # numbder of tiles per column
 TILE_SIZE = 116 # length and height of square tile
 # TILE_SIZE = 58
 # TILE_SIZE = 29
