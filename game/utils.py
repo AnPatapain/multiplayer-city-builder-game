5ac16ccfdfbbd198fff0e8b60f5f05a36e2a04bc
ke_An <nkalk192002@gmail.com>
2022-10-10 00:37:03 +0200

    add a minimap placeholder

--- game/utils.py
+++ game/utils.py
@@ -2,7 +2,7 @@ import pygame as pg
 
 def draw_text(text, size, screen, pos):
     font = pg.font.Font(None, size)
-    text_surface = font.render(text, True, (255,255, 255), None) # -> Surface
+    text_surface = font.render(text, True, (0,0, 0), None) # -> Surface
     
 
     text_rect = text_surface.get_rect(topleft=pos) # -> Rect 
