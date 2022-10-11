851906e6cbd5e6db99f0e058c7365e217e0be86d
ke_An <nkalk192002@gmail.com>
2022-10-11 14:43:11 +0200

    add task in an's task

--- game/setting.py
+++ game/setting.py
@@ -1,9 +1,9 @@
 # SCREEN_HEIGHT = 1800
 # SCREEN_WIDTH = 2880
-SCREEN_HEIGHT = 1500
-SCREEN_WIDTH = 2000
-NUMS_GRID_X = 10 # number of tiles per row 
-NUMS_GRID_Y = 10 # numbder of tiles per column
+SCREEN_HEIGHT = 1600
+SCREEN_WIDTH = 2880
+NUMS_GRID_X = 20 # number of tiles per row 
+NUMS_GRID_Y = 20 # numbder of tiles per column
 TILE_SIZE = 58 # length and height of square tile
 
 # TILE_SIZE = 58
