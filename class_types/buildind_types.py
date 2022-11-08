34f8c22792443beb9ac40b37d393746467762237
dkeme <kiki.lassa@outlook.com>
2022-11-08 15:59:08 +0100

    Fix buildingType as main

---
+++ class_types/buildind_types.py
@@ -0,0 +1,12 @@
+import enum
+
+class TypeBuilding(enum):
+    SMALL_TENT = 1
+    LARGE_TENT = 2
+    SMALL_SHACK = 3
+    LARGE_SHACK = 4
+    SENATE = 11
+    ENGINEERS_POST = 12
+    PREFECTURE = 13
+    WELL = 20
+    FOUNTAIN = 21
