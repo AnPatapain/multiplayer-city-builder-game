e92e1711746762f30ed4b46751cc320cf230b88e
dkeme <kiki.lassa@outlook.com>
2022-11-08 14:38:15 +0100

    ADD typeBuilding

--- game/Building.py
+++ game/Building.py
@@ -1,9 +1,22 @@
 import random as rd
 import string as st
+import enum
 
 def ID():
     Id = ''.join([rd.choice(st.ascii_letters + st.digits) for n in range(5)])
     return Id
+
+class typeBuilding(enum):
+    SMALL_TENT = 1
+    LARGE_TENT = 2
+    SMALL_SHACK = 3
+    LARGE_SHACK = 4
+    SENATE = 11
+    ENGINEERS_POST = 12
+    PREFECTURE = 13
+    WELL = 20
+    FOUNTAIN = 21
+    
 
 class Buildings():
     # all buildings are listed in this class
@@ -19,23 +32,3 @@ class Buildings():
         self.IsOnFire = False
         self.IsDestroyed = False
 
-
-    """
-    class ServiceBuilding(Buildings):
-    # insert here (prefecture + structure ingenieurs)
-    class Water_struct(Buildings):
-        def __init__(self, max_occupants, building_size, cost, IsOnFire, IsDestroyed, Labor):
-            Buildings.__init__(self, max_occupants, building_size, cost, IsOnFire, IsDestroyed)
-            self.Labor = Labor
-            bui_Well = (0, '1x1', 5, False, False, False, False)
-            bui_Fountain = (0, '2x2', 15, False, False, False, 4)
-    """
-
-
-
-
-
-##road =4, well = 5, clearland=2, engineer = 30, prefecture =30,fountain= 15, reservoire=80, aqueduct=8
-
-
-### water levels doc :  -False = not allowed; 1=Well allowed; 2 = ONLY the fountain is allowed ; 3 = Both Well and Fountain are allowed
