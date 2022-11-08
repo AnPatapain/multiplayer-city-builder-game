f86ca161239bb514829b31889b480951fb07b5ed
dkeme <kiki.lassa@outlook.com>
2022-11-08 15:37:00 +0100

    move TypeBuilding in class_types

--- game/Building.py
+++ game/Building.py
@@ -1,26 +1,14 @@
 import random as rd
 import string as st
-import enum
+from class_types.TypeBuilding import TypeBuilding
 
 def ID():
     Id = ''.join([rd.choice(st.ascii_letters + st.digits) for n in range(5)])
     return Id
 
-class typeBuilding(enum):
-    SMALL_TENT = 1
-    LARGE_TENT = 2
-    SMALL_SHACK = 3
-    LARGE_SHACK = 4
-    SENATE = 11
-    ENGINEERS_POST = 12
-    PREFECTURE = 13
-    WELL = 20
-    FOUNTAIN = 21
-    
-
 class Buildings():
     # all buildings are listed in this class
-    def __init__(self, max_occupants, building_size, cost, typeBuilding):
+    def __init__(self, max_occupants, building_size, cost, typeBuilding : TypeBuilding):
         ### need to add : Risk, current occupants
         self.id = ID()
         self.max_occupants = max_occupants
