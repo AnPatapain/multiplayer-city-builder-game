7b47bf52636a8ebec467f0ac5f5909dbe755e1f8
Zalax <contact@zalax.xyz>
2022-12-06 16:17:57 +0100

    perf: don't call create_static_map for nothing :')

--- game/world.py
+++ game/world.py
@@ -323,4 +323,4 @@ class World:
 
         road.set_road_connection(road_connection)
         self.grid[road_row][road_col].set_road(road)
-        self.create_static_map()
+        # self.create_static_map()
