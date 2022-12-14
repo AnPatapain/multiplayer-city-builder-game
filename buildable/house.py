3573a526690e4854695690e786ba264f6a2b6f99
DragonKEME <kiki.lassa@outlook.com>
2022-12-14 08:44:41 +0100

    fix: tax name

--- buildable/house.py
+++ buildable/house.py
@@ -2,10 +2,10 @@ from buildable.building import Buildings
 from class_types.buildind_types import BuildingTypes
 
 class Houses(Buildings):
-        def __init__(self, max_occupants, building_size : tuple, has_water, tax_multi, desirability,building_type : BuildingTypes):
+        def __init__(self, max_occupants, building_size : tuple, has_water, tax, desirability,building_type : BuildingTypes):
             Buildings.__init__(self, max_occupants, building_size, building_type)
             self.has_water = has_water
-            self.tax_multi = tax_multi
+            self.tax = tax
             self.desirability = desirability
 
         def get_has_water(self):
@@ -14,5 +14,5 @@ class Houses(Buildings):
         def set_has_water(self,has_water):
             self.has_water = has_water
 
-        def get_tax_multi(self):
-            return self.has_water
+        def get_tax(self):
+            return self.tax
