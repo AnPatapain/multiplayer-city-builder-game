6a1b3361d8462093fe88f721890303838218968d
ke_An <nkalk192002@gmail.com>
2023-01-07 23:41:57 +0100

    testing

--- buildable/final/structures/WheatFarm.py
+++ buildable/final/structures/WheatFarm.py
@@ -2,6 +2,7 @@ from buildable.structure import Structure
 from class_types.buildind_types import BuildingTypes
 from map_element.tile import Tile
 from game.game_controller import GameController
+
 class WheatFarm(Structure):
     def __init__(self, x: int, y: int) -> None:
         super().__init__(x, y, BuildingTypes.WHEAT_FARM, build_size=(3, 3), max_employee=10)
