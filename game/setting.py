d46411c52640e47778b5103d70f3aa1abe922e5a
Zalax <contact@zalax.xyz>
2023-01-07 14:23:02 +0100

    fix: remove unused settings variable

--- game/setting.py
+++ game/setting.py
@@ -1,8 +1,5 @@
-SCREEN_HEIGHT = 1600
-SCREEN_WIDTH = 2880
 GRID_SIZE = 40
-NUMS_GRID_X = 40  # number of tiles per row
-NUMS_GRID_Y = 40  # numbder of tiles per column
+
 TILE_SIZE = 58  # length and height of square tile
 
 DEFAULT_SURFACE_WIDTH = GRID_SIZE * TILE_SIZE * 2
