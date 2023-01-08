c99dc6c81cdfa84915a59427a8856441fec69f71
DragonKEME <kiki.lassa@outlook.com>
2023-01-08 18:45:15 +0100

    update senate size

--- buildable/final/structures/senate.py
+++ buildable/final/structures/senate.py
@@ -4,5 +4,5 @@ from class_types.buildind_types import BuildingTypes
 
 class Senate(Structure):
     def __init__(self, x: int, y: int):
-        super().__init__(x, y, BuildingTypes.SENATE, build_size=(3,3), max_employee=10, fire_risk=10,
+        super().__init__(x, y, BuildingTypes.SENATE, build_size=(5,5), max_employee=10, fire_risk=10,
                          destruction_risk=10)
