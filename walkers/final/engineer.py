075e38a19f7e3377344cc0a768449151d6989dbc
DragonKEME <kiki.lassa@outlook.com>
2023-01-08 06:26:08 +0100

    feat: add engineer_post with walkers

---
+++ walkers/final/engineer.py
@@ -0,0 +1,18 @@
+from buildable.buildable import Buildable
+from buildable.house import House
+from buildable.structure import Structure
+from class_types.walker_types import WalkerTypes
+from walkers.walker import Walker
+
+
+class Engineer(Walker):
+    def __init__(self, associated_building: Buildable):
+        super().__init__(WalkerTypes.ENGINEER, associated_building, max_walk_distance=10, roads_only=True)
+
+    def update(self):
+        super().update()
+        tiles = self.current_tile.get_adjacente_tiles(2)
+        for tile in tiles:
+            if tile.get_building():
+                if isinstance(tile.get_building(), House) or isinstance(tile.get_building(), Structure):
+                    tile.get_building().risk.reset_dest_risk()
