83fdac7ae050f149888e5259d1d4d6cb58a22e0a
ke_An <nkalk192002@gmail.com>
2023-01-14 23:18:35 +0100

    remove the last test for function of leo

--- buildable/final/structures/granary.py
+++ buildable/final/structures/granary.py
@@ -15,37 +15,6 @@ class Granary(Structure):
     
     def get_wheat_stocked(self): return self.wheat_stocked
 
-    # def get_all_farm_tiles(self): 
-    #     from buildable.final.structures.WheatFarm import WheatFarm
-
-    #     grid = self.game_controller.get_map()
-    #     self.wheat_farm_tiles = []
-
-    #     for row in grid:
-    #         for tile in row:
-    #             building = tile.get_building()
-    #             if isinstance(building, WheatFarm) and tile.get_show_tile():
-    #                 self.wheat_farm_tiles.append(building.get_current_tile())
-
-    #     return self.wheat_farm_tiles.copy()
-
-
-    # def new_walker(self):
-    #     if self.associated_walker:
-    #         print("A walker is already assigned to this building!")
-    #         return
-
-    #     tile = self.find_adjacent_road()
-    #     if tile:
-    #         self.associated_walker = Granary_worker(self)
-    #         self.associated_walker.spawn(tile)
-        
-
     def update_day(self):
         super().update_day()
-        print('[')
-        for tile in self.get_adjacent_tiles(radius=1):
-            print(tile.x, tile.y)
-        print(']')
-        # if not self.associated_walker:
-            # self.new_walker()
+        
