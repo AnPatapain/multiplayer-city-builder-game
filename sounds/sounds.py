0aff9b31d0b71bc864cec40594c1537620b006fc
asad <asadbek.hoshimov@insa-cvl.fr>
2023-01-28 19:15:40 +0100

    feat :sounds

--- sounds/sounds.py
+++ sounds/sounds.py
@@ -3,9 +3,9 @@ import pygame as pg
 class SoundManager:
 
     def __init__(self):
-        pg.mixer.init()
+        pg.mixer.init(44100, -16, 2, 2048)
         self.sounds = {
-            'menu_demarrer': pg.mixer.Sound("sounds/mp3/Rome4.mp3"),
+            'menu_demarrer': pg.mixer.Sound("sounds/mp3/Rome1.mp3"),
             'debut_jeu': pg.mixer.Sound('sounds/mp3/Rome1.mp3')
         }
 
