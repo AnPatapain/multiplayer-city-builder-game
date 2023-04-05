76ea1cb728be23d76c0295c9b755aaee83960e21
Zalax <contact@zalax.xyz>
2023-04-05 19:00:34 +0200

    feat: compress backups with lzma to take less space

--- backup_game.py
+++ backup_game.py
@@ -1,4 +1,5 @@
 import os
+import lzma
 import pickle
 from os import mkdir,path
 from game.game_controller import GameController
@@ -11,7 +12,7 @@ def load_game(backup_name):
     if not path.exists(path_game):
         print("game does not exists")
         return None
-    file= open(path_game, "rb")
+    file = lzma.open(path_game, "rb")
     GameController.get_instance().__dict__ = pickle.load(file)
     GameController.get_instance().save_load()
     file.close()
@@ -20,6 +21,6 @@ def save_game(backup_name):
     path_game = save_dir + backup_name
     if not path.exists(save_dir):
         mkdir(save_dir)
-    file = open(path_game, "wb")
+    file = lzma.open(path_game, "wb")
     pickle.dump(GameController.get_instance().__dict__, file)
     file.close()
