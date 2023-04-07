a82d28ff97ffb5d8f8e9f0a4c733fb2af350c909
ke_An <nkalk192002@gmail.com>
2023-04-07 16:41:52 +0200

    Test game controller sending feat

--- backup_game.py
+++ backup_game.py
@@ -3,6 +3,7 @@ import lzma
 import pickle
 from os import mkdir,path
 from game.game_controller import GameController
+from network_system.system_layer.read_write import SystemInterface
 
 save_dir = "saved_games/"
 list_fichiers = os.listdir(save_dir)
@@ -22,4 +23,11 @@ def save_game(backup_name):
         mkdir(save_dir)
     file = lzma.open(path_game, "wb")
     pickle.dump(GameController.get_instance().__dict__, file)
+
+    # ---------------------------- TEST --------------------------------
+    read_write_py_c = SystemInterface.get_instance()
+    serialize_data = pickle.dumps(GameController.get_instance().__dict__)
+    read_write_py_c.send_message(id_player=1, command=2, id_object=3, data=serialize_data, encode=False)
+    # -------------------------- FIN TEST ------------------------------
+
     file.close()
